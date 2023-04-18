ea5c81c31b78ea9c50c0d53ef2cd163c7f065569
Alexandre Campos <alexandreccampos1@gmail.com>
2023-04-17 21:56:17 -0300

    update1
    Co-authored-by: vdvdlk <vdvdlk@users.noreply.github.com>

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import norm #gaussian plot
 
 rng = np.random.default_rng(seed=1)
 
@@ -8,6 +8,8 @@ x_0 = 1.0
 dt = 0.01
 n = 10000
 epsilon = 0.25
+a = 1.0
+A = 0.12
 
 t = np.arange(stop=n) * dt
 x = np.zeros(
@@ -20,7 +22,6 @@ x[0] = x_0
 def F(x, t, a=1.0, A=0.12, Omega=1e-3):
     return x * (a - x**2) + A * np.cos(Omega * t)
 
-
 # def G(x, epsilon=1.0):
 #     return epsilon
 
@@ -30,11 +31,8 @@ def F(x, t, a=1.0, A=0.12, Omega=1e-3):
 #     a=valores_eta,
 #     size=n
 # )
-eta_t = np.random.normal(0, 1./np.sqrt(epsilon), n)
+eta_t = rng.normal(0, 1./np.sqrt(epsilon), n)
 
-
-a = 1.0
-A = 0.12
 for i in range(n - 1):
     forca = F(x[i], t[i])
     t[i + 1] = t[i] + dt
@@ -43,10 +41,29 @@ for i in range(n - 1):
 
 fig, ax = plt.subplots()
 ax.set_xlim(0, 100)
-ax.set_ylim(-2, 2)
+ax.set_ylim(-5, 5)
 
 ax.plot(t, x)
 
 fig.savefig('teste_sorteio_gaussiano.png')
 
-# blah
+
+#plotting gaussian to show the rnd gen is in fact gaussian
+# Plotting the histogram.
+fig2, ax2 = plt.subplots()
+ax2.hist(eta_t, bins=25, density=True, alpha=0.6, color='b')
+
+mu, std = norm.fit(eta_t) 
+xmin, xmax = ax2.get_xlim()
+# x = np.linspace(xmin, xmax, 1)
+x = np.arange(xmin, xmax, 0.1)
+# p = norm.pdf(x, 0,  1./np.sqrt(epsilon))
+print(1./np.sqrt(epsilon))
+p = norm.pdf(x, mu, std)
+
+ax2.plot(x, p, 'k', linewidth=2)
+title = "Fit Values: {:.2f} and {:.2f}".format(mu, std)
+ax2.set_title(title)
+fig2.savefig('gaussiana.png')
+
+# ------
