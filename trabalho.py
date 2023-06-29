da50846c23a7b34e0abfa174b9c7e5efdb839061
vdvdlk <thalesfreitas96@gmail.com>
2023-06-29 20:02:25 -0300

    Simulação melhorada

--- trabalho.py
+++ trabalho.py
@@ -6,18 +6,26 @@ rng = np.random.default_rng(seed=0)
 
 
 def dU_dT(T, U0, T1, T2):
-    return 32 * U0 * (T - T1) * (T - T2) * (2 * T - T1 - T2) / (T1 - T2)**4
+    return 32 * U0 * (T - T1) * (T - T2) * (2 * T - T1 - T2) / (T1 - T2) ** 4
+
+
+def d2U_dT2(T, U0, T1, T2):
+    return (
+        32
+        * (6 * T**2 - 6 * T * T1 + T1**2 - 6 * T * T2 + 4 * T1 * T2 + T2**2)
+        * U0
+        / (T1 - T2) ** 4
+    )
 
 
 # Parâmetros
-U0 = 1e0
 T1 = 280.0
 T2 = 290.0
-g = 0.5
+g = 1.0
+U0 = g**2/(2 * 0.12)  # 1e0
 periodo = 1e5
 omega = 2 * np.pi / periodo
-epsilon = 0.055
-
+epsilon = 0.001 * 340
 dt = 0.01
 t_f = 300.0 * 1e3
 n = int(t_f // dt)
@@ -25,9 +33,7 @@ n = int(t_f // dt)
 t = dt * np.arange(n)
 
 T_0 = np.copy(T2)
-T = np.zeros(
-    shape=n
-)
+T = np.zeros(shape=n)
 T[0] = T_0
 
 # eta = rng.choice(
@@ -50,8 +56,8 @@ dW = rng.normal(
 # Prescrição de Itô
 # T(t + dt) = T(t) + dt * ( -U'(T) + g * eta(t) )
 # dT = dt * ( -U'(T)) + g * dW
-for i in trange(n - 1, desc='Trabalho'):
-    F_i = - dU_dT(T[i], U0, T1, T2) - epsilon * np.cos(omega * t[i])
+for i in trange(n - 1, desc="Trabalho"):
+    F_i = -dU_dT(T[i], U0, T1, T2) - epsilon * np.cos(omega * t[i])
     T[i + 1] = T[i] + dt * F_i + g * dW[i]
 
 
@@ -61,10 +67,10 @@ fig, ax = plt.subplots()
 
 ax.grid(visible=True)
 
-ax.set_xlabel('Anos ($\\times 10^3$)')
+ax.set_xlabel("Anos ($\\times 10^3$)")
 ax.set_xlim(0, t_f / 1e3)
 
-ax.set_ylabel('Temperatura (K)')
+ax.set_ylabel("Temperatura (K)")
 ax.set_ylim(T1 - 10, T2 + 10)
 
 ax.plot(
@@ -74,7 +80,7 @@ ax.plot(
 )
 
 fig.savefig(
-    fname='trabalho.pdf',
+    fname="trabalho.pdf",
     dpi=300,
 )
 
