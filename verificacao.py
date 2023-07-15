db9b0dd12bb3f3c4d7ecf7768c7e20e40bba52fe
vdvdlk <thalesfreitas96@gmail.com>
2023-07-14 22:36:39 -0300

    Multiprocesso

--- verificacao.py
+++ verificacao.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import trange
 import pickle
-import multiprocessing
+from multiprocessing import Pool
 
 from trabalho import (
     rng,
@@ -26,14 +26,11 @@ from trabalho import (
 # array_epsilon = np.linspace(20-10, 20+10, num=5)
 # array_g = np.linspace(5.6-2, 5.6+2, num=5)
 
-array_U0 = np.array([0.1, 1, 10, 100, 1000])
-array_g = np.array([0.1, 10, 20, 30, 40])
+array_U0 = np.array([0.1, 1, 10])
+array_g = np.array([0.1, 10, 20])
 
 # array_U0 = np.array([1, U0])
 # array_g = np.array([1, g])
-
-T = np.zeros(shape=n)
-T[0] = T1
 
 fig, axs = plt.subplots(nrows=array_U0.size, ncols=array_g.size)
 # fig.set_size_inches(10, 10)
@@ -64,7 +61,12 @@ fig.savefig(
 )
 
 
-def f(j, k):
+def f(args):
+    j = args[0]
+    k = args[1]
+    T = np.zeros(shape=n)
+    T[0] = T1
+
     U0 = array_U0[j]
     g = array_g[k]
 
@@ -74,12 +76,27 @@ def f(j, k):
     for i in trange(n - 1, desc="Subplot " + str(j) + " " + str(k)):
         F_i = -dU_dT(T[i], U0, T1, T2) - epsilon * np.cos(omega * t[i])
         T[i + 1] = T[i] + dt * F_i + g * dW[i]
+    
+    return T
 
-    passo = 100000
-    axs[j, k].plot(
-        (t / 1e3)[::passo],
-        T[::passo],
-    )
+if __name__ == '__main__':
+    with Pool(4) as p:
+        u = p.map(
+             func=f,
+             iterable=np.ndindex(array_U0.size, array_g.size)
+         )
+        lista_u = list(u)
+
+
+    for m in range(array_U0.size * array_g.size):
+        j, k = list(np.ndindex(array_U0.size, array_g.size))[m]
+        T_m = lista_u[m]
+
+        passo = 100000
+        axs[j, k].plot(
+            (t / 1e3)[::passo],
+            T_m[::passo],
+        )
 
     fig.savefig(
         fname="verificacao.pdf",
